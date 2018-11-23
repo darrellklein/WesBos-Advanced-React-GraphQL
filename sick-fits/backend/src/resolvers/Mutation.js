dcfa6fe3e3649beda4afddec8ff7881d921512e4
Darrell Klein <darrelldklein@gmail.com>
2018-11-23 15:18:52 -0800

    first query and mutation created (get items and create an item)

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        //TODO: authentication
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+};
 
 module.exports = Mutations;
